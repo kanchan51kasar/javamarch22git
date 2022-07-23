ddaa6cba86532e83759499d25d198a5fcc93e6d3
kanchan <kanchan@DESKTOP-KCAL9TF>
2022-07-23 14:40:01 +0530

    second commit

--- src/com/aspire/java/sample/First.java
+++ src/com/aspire/java/sample/First.java
@@ -5,7 +5,7 @@ public class First
 	public static void main(String[] args)
 	{
 		System.out.println("Hi This is my first push on git hub");
-		
+		System.out.println("second change in second program");
 	}
 
 }
